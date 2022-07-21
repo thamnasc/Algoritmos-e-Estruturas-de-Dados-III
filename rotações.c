330b21e6f6acd11b8e88f07e790500ba2044a74a
thamnasc <thalitanas09@gmail.com>
2022-07-21 19:33:48 -0300

    Atualiza rotações

--- rotações.c
+++ rotações.c
@@ -5,15 +5,16 @@ typedef struct tNo {
     int valor;
     struct tNo *pai, *esq, *dir;
 } tNo;
+
 tNo *rotacaoDir(tNo *x) {
     
-    tNo *y = x->pai;
-    y->esq = x->dir;
-    if(x->dir != NULL)
-        x->dir->pai = y;
-    x->dir = y;
-    x->pai = y->pai;
-    y->pai = x;
+    tNo *y = x->esq;
+    x->esq = y->dir;
+    y->pai = x->pai;
+    x->pai = y;
+    if(y->dir != NULL)
+        y->dir->pai = x;
+    y->dir = x;
 
     return y;
 }
@@ -22,11 +23,11 @@ tNo *rotacaoEsq(tNo *x) {
 
     tNo *y = x->dir;
     x->dir = y->esq;
+    y->pai = x->pai;
+    x->pai = y; 
     if(y->esq != NULL)
         y->esq->pai = x;
     y->esq = x;
-    y->pai = x->pai;
-    x->pai = y;
 
     return y;
 }
