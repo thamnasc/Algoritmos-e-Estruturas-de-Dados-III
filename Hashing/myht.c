afe3dde5f03e3970ee4127dd7436035f2e6f830c
thamnasc <thalitanas09@gmail.com>
2022-08-13 13:26:08 -0300

    ajusta inserção

--- Hashing/myht.c
+++ Hashing/myht.c
@@ -7,41 +7,48 @@ typedef struct hashTable {
     int deleted;
 } hashTable;
 
-int h1(int k) {
+int h1( int k ) {
     return k % m;
 }
 
-int h2(int k, int i) {
-    return floor(m * (k * 0.9 - floor(k * 0.9)));
+int h2( int k, int i ) {
+    return floor( m * ( k * 0.9 - floor( k * 0.9 ) ) );
     /*return floor(m * ((k + i) * 0.9 - floor((k + i) * 0.9)));*/
 }
 
-/* TODO: ajustar para ignorar chaves repetidas */
-int insert(int key, hashTable ht1, hashTable ht2) {
-
-    int index, i = 0;
-
-    index = h1(key);
-    if(ht1[index].key == NULL || ht1[index].deleted) 
-        ht1[index] = key;
-    else {
-        do {
-            index = h2(key, i);
-            if(ht2[index].key == NULL || ht2[index].deleted) {
-                ht2[index].key = key;
-                if(ht2[index].deleted)
-                    ht2[index].deleted = 0;
-                return 
-            }
-            i++;
-        } while ( ( i < m ) && ( ht2[index].key != NULL || ) ); /* sondagem caso ocorra colisão em ht2 */
+void delete( int key ) {
     
-    return NULL; /* não há espaço para alocar outras chaves */
-}
-        
 }
 
-int lookup(int key, hashTable ht1, hashTable ht2) {
+int insert( int key, hashTable ht1, hashTable ht2 ) {
+
+    int index, auxKey, i = 0;
+
+    index = lookup( key, ht1, ht2 ); /* procura se a chave já existe */
+    if( index == NULL ) { /* apenas se a chave não existir tenta inserir */
+        index = ht1( key );
+        if( ht1[index].key == NULL || ht1[index].deleted ) 
+            ht1[index].key = key;
+        else { /* em caso de colisão */
+            auxKey = ht1[index].key; /* salva chave de ht1 para sobrescrever pela chave mais recente */
+            ht1[index].key = key; 
+            do { /* procura por slot em ht2 */
+                index = h2( auxKey, i );
+                if( ht2[index].key == NULL || ht2[index].deleted ) {
+                    ht2[index].key = key;
+                    ht2[index].deleted = 0;
+                    return index;
+                }
+                i++;
+            } while ( ( i < m ) && ( ht2[index].key != NULL ) ); /* sondagem caso ocorra colisão em ht2 */
+        
+            return NULL; /* não há espaço para alocar outras chaves */
+        }
+    }
+    return index; /* retorna o índice da chave já existente */
+}
+
+int lookup( int key, hashTable ht1, hashTable ht2 ) {
     
     int index, i = 0;
 
@@ -65,7 +72,7 @@ void initializeHash(hashTable ht) {
     
     int i = 0;
 
-    for(i; i < m; i++) {
+    for( i; i < m; i++ ) {
         ht[i].key = NULL;
         ht[i].deleted = 0;
     }
@@ -75,8 +82,8 @@ int main() {
 
     hashTable ht1[m], ht2[m];
 
-    initializeHash(ht1);
-    initializeHash(ht2);
+    initializeHash( ht1 );
+    initializeHash( ht2 );
 
     return 0;
 }
